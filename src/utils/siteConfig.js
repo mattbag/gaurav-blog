edef610e4891742c3e4d29c28db468b837146d0b
matt <matt@atlasagency.com.au>
2018-09-26 15:39:25 +1000

    meta site

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,21 +1,21 @@
 module.exports = {
-  siteTitle: 'Gatsby',
-  siteTitleAlt: 'GCN Gatsby Starter', // This allows an alternative site title for SEO schema.
-  publisher: 'Publisher named GCN', // Organization name used for SEO schema
+  siteTitle: 'SEOSmash',
+  siteTitleAlt: 'Smash the serach engines with our tips', // This allows an alternative site title for SEO schema.
+  publisher: 'SEOSmash Inc.', // Organization name used for SEO schema
   siteDescription:
-    'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
+    'Smash the serach engines with our tips',
   siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
   postsPerHomePage: 10, // Number of posts shown on the 1st page of of the index.js template (home page)
   postsPerPage: 12, // Number of posts shown on paginated pages
-  author: 'GCN User', // Author for RSS author segment and SEO schema
+  author: 'SEOSmasher', // Author for RSS author segment and SEO schema
   authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@twitter', // Change for Twitter Cards
-  shortTitle: 'GCN App', // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: 'SEOSmash', // Used for App manifest e.g. Mobile Home Screen
   shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
   shareImageWidth: 900, // Change to the width of your default share image
   shareImageHeight: 600, // Change to the height of your default share image
   siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
   backgroundColor: '#f0f1ff', // Used for Offline Manifest
   themeColor: '#f0f1ff', // Used for Offline Manifest
-  copyright: 'Copyright © 2018 GCN User', // Copyright string for the RSS feed
+  copyright: 'Copyright © 2018 SEOSmash', // Copyright string for the RSS feed
 }
