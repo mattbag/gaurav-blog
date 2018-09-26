07210f5a7835fbce9b6d85683f2e88d2f6a0dc00
matt <matt@atlasagency.com.au>
2018-09-26 15:25:57 +1000

    share state

--- src/components/share.js
+++ src/components/share.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components'
 import {
     FacebookShareButton,
@@ -44,62 +44,78 @@ const ShareList = styled.ul`
         }
     }
 `
-const icon={
+const icon = {
     size: 32
 }
-const Share = (props) => typeof window !== 'undefined' &&
-    <ShareList>
-        <li>
-            <FacebookShareButton url={location && location.href}>
-                <FacebookIcon size={icon.size} />
-            </FacebookShareButton>
-        </li>
-        <li>
-            <TwitterShareButton url={location && location.href}>
-                <TwitterIcon size={icon.size} />
-            </TwitterShareButton>
-        </li>
-        <li>
-            <LinkedinShareButton url={location && location.href}>
-                <LinkedinIcon size={icon.size} />
-            </LinkedinShareButton>
-        </li>
-        <li>
-            <WhatsappShareButton url={location && location.href}>
-                <WhatsappIcon size={icon.size} />
-            </WhatsappShareButton>
-        </li>
-        <li>
-            <TelegramShareButton url={location && location.href}>
-                <TelegramIcon size={icon.size} />
-            </TelegramShareButton>
-        </li>
-        <li>
-            <GooglePlusShareButton url={location && location.href}>
-                <GooglePlusIcon size={icon.size} />
-            </GooglePlusShareButton>
-        </li>
-        <li>
-            <EmailShareButton url={location && location.href}>
-                <EmailIcon size={icon.size} />
-            </EmailShareButton>
-        </li>
-        <li>
-            <RedditShareButton url={location && location.href}>
-                <RedditIcon size={icon.size} />
-            </RedditShareButton>
-        </li>
-        <li>
-            <LivejournalShareButton url={location && location.href}>
-                <LivejournalIcon size={icon.size} />
-            </LivejournalShareButton>
-        </li>
-        <li>
-            <WorkplaceShareButton url={location && location.href}>
-                <WorkplaceIcon size={icon.size} />
-            </WorkplaceShareButton>
-        </li>
-    </ShareList>
-;
 
-export default Share
+class Share extends Component {
+    state = { isClient: false }
+
+    componentDidMount() {
+            this.setState({isClient:true})
+    }
+
+    render() {
+        return (
+            this.state.isClient &&
+            <ShareList>
+                <li>
+                    <FacebookShareButton url={location && location.href}>
+                        <FacebookIcon size={icon.size} />
+                    </FacebookShareButton>
+                </li>
+                <li>
+                    <TwitterShareButton url={location && location.href}>
+                        <TwitterIcon size={icon.size} />
+                    </TwitterShareButton>
+                </li>
+                <li>
+                    <LinkedinShareButton url={location && location.href}>
+                        <LinkedinIcon size={icon.size} />
+                    </LinkedinShareButton>
+                </li>
+                <li>
+                    <WhatsappShareButton url={location && location.href}>
+                        <WhatsappIcon size={icon.size} />
+                    </WhatsappShareButton>
+                </li>
+                <li>
+                    <TelegramShareButton url={location && location.href}>
+                        <TelegramIcon size={icon.size} />
+                    </TelegramShareButton>
+                </li>
+                <li>
+                    <GooglePlusShareButton url={location && location.href}>
+                        <GooglePlusIcon size={icon.size} />
+                    </GooglePlusShareButton>
+                </li>
+                <li>
+                    <EmailShareButton url={location && location.href}>
+                        <EmailIcon size={icon.size} />
+                    </EmailShareButton>
+                </li>
+                <li>
+                    <RedditShareButton url={location && location.href}>
+                        <RedditIcon size={icon.size} />
+                    </RedditShareButton>
+                </li>
+                <li>
+                    <LivejournalShareButton url={location && location.href}>
+                        <LivejournalIcon size={icon.size} />
+                    </LivejournalShareButton>
+                </li>
+                <li>
+                    <WorkplaceShareButton url={location && location.href}>
+                        <WorkplaceIcon size={icon.size} />
+                    </WorkplaceShareButton>
+                </li>
+            </ShareList>
+        );
+    }
+}
+
+// Share.propTypes = {
+
+// };
+
+export default Share;
